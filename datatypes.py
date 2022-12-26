f7646a486c8c31df879e29ec9e2d72d3c76b9381
unknown <dheepi117@gmail.com>
2022-12-26 12:32:44 +0530

    adding file.

---
+++ datatypes.py
@@ -0,0 +1,22 @@
+#integer
+num=117
+print(type(num))
+print(num)
+
+#float
+a=5.14
+print(type(a))
+print(a)
+
+#string
+c="apple"
+print(type(c))
+print(c)
+
+#boolean
+a = 20
+b = 30
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
